9ea5f8d0467b2ee0f354099b74da481402899aa1
RuxsoraPardayeva <250268@newuu.uz>
2025-09-25 12:54:27 +0500

    Update classwork.cpp
    changed

--- classwork.cpp
+++ classwork.cpp
@@ -7,247 +7,171 @@ int main() {
 
 
 
-    //problem 12
-
-    // int a,b,c;
-    // cin >> a;
-    // cin >> b;
-    // cin >> c;
-    // if (a+b+c==180) {
-    //     cout << "this triangle is valid"<< endl;
-    // }
+    //problem 11
+    // int total=0;
+    // int pos=0;
+    // int neg=0;
     //
-    // else {
-    //     cout << "this triangle is not valid"<< endl;
+    //
+    // while (true) {
+    //     int number;
+    //     cin>>number;
+    //     if (number<0) {
+    //         neg++;
+    //        total++;
+    //     }
+    //     else if (number>0) {
+    //         pos++;
+    //         total++;
+    //     }
+    //     else {
+    //         total++;
+    //         break;
+    //     };
     // }
+    // cout<<"Total: "<<total<<endl;
+    // cout<<"Positive numbers: "<<pos<<endl;
+    // cout<<"Negative numbers: "<<neg<<endl;
+
+
+    //problem 12
+    // int a;
+    // cin >> a;
+    // for (int i = 0; i < a; i++) {
+    //     for (int j = 0; j < a; j++) {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
 
     //problem 13
-    // float a,b,c;
-    // cin >> a;
-    // cin >> b;
-    // cin >> c;
-    //
-    // if (b*b-4*a*c>0) {
-    //     cout << "x1 = " <<(-b+sqrt(b*b-4*a*c))/(2*a)<< endl;
-    //     cout << "x2 = " <<(-b-sqrt(b*b-4*a*c))/(2*a)<< endl;
-    // }
-    //
-    // else if (b*b-4*a*c==0){
-    //     cout << "x1 = " <<(-b+sqrt(b*b-4*a*c))/(2*a)<< endl;
-    // }
-    // else {
-    //     cout<<"root does not exist"<<endl;
-    // }
 
+    // int n;
+    // cin>>n;
+    // for (int i = 1; i < n+1; i++) {
+    //     for (int j = 0; j < i; j++) {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
 
 
     //problem 14
-    // char a;
-    // cin >> a;
-    //
-    // if (a>65 && a<90) {
-    //     cout <<" Uppercase alphabet"<< endl;
+    // int n;
+    // cin>>n;
+    // if (n==0) {
+    //     cout<<"The debth is 0";
     // }
-    // else if (a>97 && a<122) {
-    //     cout << "Lowercase alphabet"<< endl;
-    //
-    // }
-    // else {
-    //     cout << "It is not an alphabet"<< endl;
-    // }
-
+    // else{
+    // for (int i = 1; i < n+1; i++) {
+    //     for (int j = 0; j < i; j++) {
+    //         cout<<i;
+    //     }
+    //     cout<<endl;
+    // }}
 
     //problem 15
-
-    // float weight1;
-    // cout << "Enter weight and price for package 1 " << endl;
-    // cin >> weight1;
-    // float price1;
-    // cin >> price1;
-    //cout << "Enter weight and price for package 2 " << endl;
-    // float weight2;
-    // cin >> weight2;
-    // float price2;
-    // cin >> price2;
+    // cout << "Hello World!" <<1/5<< endl;
+    // float n;
+    // cin>>n;
+    // float b=1;
+    // float total=0;
+    // for (int i = 1; i < n+1; i++) {
+    //     if (i==n) {
+    //         total += (b/i);
+    //         cout<<"1/"<<i<<"=";
+    //     }
+    //     else {
+    //         total += (b/i);
+    //         cout<<"1/"<<i<<"+";
+    //     }
     //
-    // if (price1/weight1<price2/weight2) {
-    //     cout << "Package 1 has a better price"<< endl;
-    // }
-    // else if (price2/weight2<price1/weight1) {
-    //     cout << "Package 2 has a better price"<< endl;
     //
-    // }
-    // else {
-    //     cout <<" They have equal price"<< endl;
-    // }
-
+    //
+    // };
+    // cout<<total<<endl;
+    //
+    // cout<<"the sum is "<<total<<endl;
 
     //problem 16
-    // cout << "enter three digit number " << endl;
-    // int n;
-    // cin >> n;
-    // if (n/100==n%10) {
-    //
-    //     cout << n<< " is polindrome" << endl;
-    // }
-    //
-    // else  {
-    //     cout << n<< "  is not polindrome" << endl;
-    // }
 
-
+    // int year;
+    // cin>>year;
+    // float total=0;
+    // float rent=1000;
+    // for (int i = 1; i <= year; i++) {
+    //     if (i==1) {
+    //         total+=(rent*12);
+    //         cout<<"For year number "<<i<<" rent is "<<rent*12<<endl;
+    //         cout<<"Total for year "<<i<<" is "<<total<<endl;
+    //     }
+    //     else {
+    //         rent=(rent*1.03);
+    //         total+=(rent*12);
+    //         cout<<"For year number "<<i<<" rent is "<<rent*12<<endl;
+    //         cout<<"Total for year "<<i<<" is "<<total<<endl;
+    //     }
+    // }
 
     //problem 17
-    // cout << "u - Uzbek " << endl;
-    // cout << "e - English " << endl;
-    // cout << "r - Russian " << endl;
-    // cout << "g - German " << endl;
-    //
-    // char a;
-    // cin >> a;
-    // if (a == 'u') {
-    //     cout << "Assalomu alykum" << endl;
-    // }
-    // else if (a == 'e') {
-    //     cout << "Hello " << endl;
-    //
-    // }
-    // else if (a == 'r') {
-    //     cout << "привет" << endl;
-    // }
-    // else if (a == 'g') {
-    //     cout<<"Hallo"<<endl;
-    //
-    // }
-    // else {
-    //     cout << "i don't know this language" << endl;
-    // }
 
-    //problem 18
-    // float x,y;
-    // cin>>x>>y;
-    // if (sqrt(x*x+y*y) <= 10) {
-    //     cout<<"Point ("<<x<<","<<y<<") is in  the circle"<<endl;
+    // int num;
+    // cin >> num;
+    // int total=0;
+    // int o=0;
+    // int b=1;
+    // int i=1;
+    // while(i!=num) {
+    //     int t = pow(10,o);
+    //     i=num%t;
+    //     b=num/t;
+    //     total+=b%10;
+    //
+    //     o++;
     // }
-    // else {
-    //     cout<<"Point ("<<x<<","<<y<<") is not in  the circle"<<endl;
-    // }
+    // cout << "Sum of the digits is "<< total << endl;
 
+   //problem 18
+
+    // string bString;
+    // cout << "Enter a binary number: ";
+    // cin >> bString;
+    // cout << "The decimal equivalent is: " << stoi(bString, nullptr, 2)<< endl;
 
     //problem 19
-    // float n;
-    // cin >> n;
-    // int b;
-    // b=n*10;
-    // switch(b/5){
+    // int num;
+    // cout << "Enter a positive integer: ";
+    // cin >> num;
+    // cout << "Factors of " << num << " are: ";
     //
-    //         case  9:
-    //             cout << "You got 80% scholarship";
-    //             break;
-    //         case 8:
-    //             cout << "You got 80% scholarship";
-    //             break;
-    //         case  7:
-    //             cout << "You got 60% scholarship";
-    //             break;
-    //         case  6:
-    //             cout << "You got 50% scholarship";
-    //             break;
-    //         case 5:
-    //             cout << "You got 0% scholarship";
-    //             break;
-    //         case 4:
-    //             cout << "You got 0% scholarship";
-    //             break;
-    //         case 3:
-    //           cout << "You got 0% scholarship";
-    //              break;
-    //         case 2:
-    //             cout << "You got 0% scholarship";
-    //             break;
     //
-    //         case 1:
-    //             cout << "You got 0% scholarship";
-    //            break;
-    //         case 0:
-    //            cout << "You got 0% scholarship";
-    //            break;
+    // for (int i = 1; i <= num; ++i) {
     //
+    //     if (num % i == 0) {
+    //      cout << i << " ";
     //     }
-
-    //problem 20
-    int a;
-    cin >> a;
+    // }
+    //  cout << std::endl;
 
 
-    if (a<0) {
-        cout <<"Invalid input"<< endl;
-    }
-    else if (a>0 && a<=1) {
-        cout<<"3500"<<endl;
-
-    }
-    else if (a>1 && a<=3) {
-        cout<<"5500"<<endl;
-    }
-    else if (a>3 && a<=10) {
-        cout<<"8500"<<endl;
-    }
-    else if (a>10 && a<=20) {
-        cout<<"10500"<<endl;
-    }
-    else {
-        cout << "The package can not be shifted"<< endl;
-    }
-
-
-
-
-
-    //problem 21
-    int b;
-    cin >> b;
-    switch(b){
-
-            case  1:
-            cout << "1-yanvar – Yangi yil (global bayram, ammo O'zbekistonda ham keng nishonlanadi) 14-yanvar – Vatan himoyachilari kuni";
-                break;
-            case 2:
-            cout << "14-fevral – Sevgililar kuni (bu bayram jahonda keng tarqalgan bo'lsa-da, O'zbekistonda ham nishonlanadi) 21-fevral – Xalqaro ona tili kuni";
-                break;
-            case  3:
-            cout << "8-mart – Xalqaro ayollar kuni 21-23 mart – Navro'z (O'zbekistondagi eng katta milliy bayramlardan biri)";
-                break;
-            case  4:
-            cout << "9-aprel – O'zbekiston Respublikasi davlat ramzlari kuni 23-aprel – Xalqaro kitob va mualliflar kuni (bu bayram O'zbekistonda keng nishonlanadi)";
-                break;
-            case 5:
-            cout << "9-may – Xotira va qadrlash kuni 1-may – Mehnat kuni";
-                break;
-            case 6:
-            cout << "1-iyun – Xalqaro bolalar kuni 20-iyun – O'zbekiston Respublikasining davlat mustaqilligi kuni (O'zbekistonda maxsus nishonlanadi) ";
-                break;
-            case 7:
-              cout << "20-iyul – O'zbekiston Respublikasining do'stlik va hamkorlik bayrami";
-                 break;
-            case 8:
-                cout << "31-avgust – O'zbekiston Mustaqilligi kuni (O'zbekistonda eng yirik va eng muhim bayramlardan biri)";
-                break;
-
-            case 9:
-            cout << "1-sentabr – Bilimlar kuni";
-               break;
-            case 10:
-            cout << "1-oktyabr – Xalqaro qariyalar kuni 14-oktyabr – O'zbekiston Respublikasining davlat mustaqilligi kuni";
-               break;
-            case 11:
-            cout << "9-noyabr – O'zbekiston Respublikasi Milliy bayrog'i kuni 4-noyabr – Mustaqillik bayrami";
-                break;
-            case 12:
-            cout << "8-dekabr – O'zbekiston Respublikasining Konstitutsiyasi kuni 31-dekabr – Yangi yil kechasi";
-                break;
-
-        }
+    //  problem 24
+    // int even =0;
+    // int odd = 0;
+    // for (int i = 1; i <= 100000; i++) {
+    //     int b;
+    //     b=rand();
+    //     if (b%2==0) {
+    //         even++;
+    //     }
+    //     else {
+    //         odd++;
+    //     }
+    // }
+    // cout <<"number of even numbers" <<even << endl;
+    // cout << "numbers of odd numbers"<<odd << endl;
+    //
+    // return 0;
 
 
     return 0;
